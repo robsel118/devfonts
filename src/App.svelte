ad911c701612daa36fab90e3191bf1b84df86f15
robsel118 <robert.seligmann@protonmail.ch>
2020-12-28 15:46:13 +0100

    added menu to separate component

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,12 @@
 <script lang="ts">
-	import {
-		Select,
-		SelectItem,
-		Checkbox,
-		Toggle,
-	} from "carbon-components-svelte";
 	import "carbon-components-svelte/css/g10.css";
 	import FontPreviewer from "./components/FontPreviewer.svelte";
+	import Menu from "./components/Menu.svelte";
 	import { generateFontFace } from "./utils/fonts";
-	import { modes } from "./utils/modes";
-	import { themes } from "./utils/themes";
-	import { theme, mode, pinnedFonts } from "./stores";
+
+	import { theme, pinnedFonts } from "./stores";
 	import { fontFamilies } from "./utils/fonts";
 	import { Filter } from "./utils/filters";
-	import _ from "lodash";
 	let filterByLigatures: false;
 	let filterByFreeFonts: false;
 	let filterByPinnedFonts: false;
@@ -37,10 +30,6 @@
 		main {
 			max-width: 1200px;
 		}
-
-		.menu {
-			grid-template-columns: repeat(2, 1fr);
-		}
 	}
 
 	.list {
@@ -50,13 +39,6 @@
 		justify-content: space-between;
 		gap: 1rem;
 	}
-	.menu {
-		display: grid;
-		gap: 1.5rem;
-		margin: 2rem 0;
-		padding: 1rem 0;
-		border-bottom: 1px solid #ddd;
-	}
 </style>
 
 <svelte:head>
@@ -65,29 +47,10 @@
 		href={`https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.52.2/theme/${$theme}.css`} />
 </svelte:head>
 
-{@html `<style>${styles}</style>`}
+{@html `<style ✂prettier:content✂="JHtzdHlsZXN9"></style>`}
 
 <main>
-	<div class="menu">
-		<Select bind:selected={$theme} light labelText="Theme">
-			{#each themes as theme}
-				<SelectItem value={theme} text={theme} />
-			{/each}
-		</Select>
-
-		<Select bind:selected={$mode} light labelText="Mode">
-			{#each modes as mode}
-				<SelectItem value={mode} text={mode} />
-			{/each}
-		</Select>
-
-		<Checkbox labelText="Ligatures" bind:checked={filterByLigatures}  />
-		<Checkbox labelText="Free Fonts" bind:checked={filterByFreeFonts} />
-		<Toggle
-			labelText="Display only pinned fonts"
-			bind:toggled={filterByPinnedFonts} />
-	</div>
-
+	<Menu bind:filterByPinnedFonts bind:filterByLigatures bind:filterByFreeFonts/>
 	<div class="list">
 		{#each fontList as fontFamily}
 			<FontPreviewer font={fontFamily} />
---
+++ src/components/Menu.svelte
@@ -0,0 +1,54 @@
+<script>
+    import {
+        Select,
+        SelectItem,
+        Checkbox,
+        Toggle,
+    } from "carbon-components-svelte";
+    import { modes } from "../utils/modes";
+    import { themes } from "../utils/themes";
+	import { theme, mode } from "../stores";
+
+
+    export let filterByLigatures;
+	export let filterByFreeFonts;
+	export let filterByPinnedFonts;
+</script>
+
+<style>
+    .menu {
+        display: grid;
+        gap: 1.5rem;
+        margin: 2rem 0;
+        padding: 1rem 0;
+        border-bottom: 1px solid #ddd;
+    }
+
+
+	@media (min-width: 640px) {
+
+		.menu {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+</style>
+
+<div class="menu">
+    <Select bind:selected={$theme} light labelText="Theme">
+        {#each themes as theme}
+            <SelectItem value={theme} text={theme} />
+        {/each}
+    </Select>
+
+    <Select bind:selected={$mode} light labelText="Mode">
+        {#each modes as mode}
+            <SelectItem value={mode} text={mode} />
+        {/each}
+    </Select>
+
+    <Checkbox labelText="Ligatures" bind:checked={filterByLigatures} />
+    <Checkbox labelText="Free Fonts" bind:checked={filterByFreeFonts} />
+    <Toggle
+        labelText="Display only pinned fonts"
+        bind:toggled={filterByPinnedFonts} />
+</div>
